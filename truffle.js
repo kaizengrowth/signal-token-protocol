3ac0cd53949468695b2ac7ddf06778b149d6fe90
Aaron Shafron <ahshafron@gmail.com>
2018-01-31 08:12:52 -0500

    configure Infura connection

--- truffle.js
+++ truffle.js
@@ -1,9 +1,19 @@
+import INFURA_API_KEY from './config'
+import MNEMONIC from './config'
+
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
 module.exports = {
   networks: {
-    "ropsten": {
-      network_id: 1,
+    development: {
+      network_id: 5777,
       host: "localhost",
       port: 8545
+    },
+    ropsten:  {
+      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"+infura_apikey),
+      network_id: 3,
+      gas: 3000000
     }
   }
 };
