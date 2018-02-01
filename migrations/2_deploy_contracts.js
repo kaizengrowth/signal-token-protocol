f820df0e3f9a5a42a9d258fb788ff6f0926015c8
Adam Gall <adam@decentcrypto.com>
2018-01-31 22:27:30 -0500

    Add mainnet deployment block to "deploy contracts" migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,7 +2,11 @@ const SignalToken = artifacts.require("./SignalToken.sol");
 const SignalTokenProtocol = artifacts.require("./SignalTokenProtocol.sol");
 
 module.exports = function(deployer, network, accounts) {
-  deployer.deploy(SignalToken).then(() => {
-    return deployer.deploy(SignalTokenProtocol, SignalToken.address);
-  });
+  if (network == "live") {
+    return deployer.deploy(SignalTokenProtocol, "0x6888a16ea9792c15a4dcf2f6c623d055c8ede792");
+  } else {
+    deployer.deploy(SignalToken).then(() => {
+      return deployer.deploy(SignalTokenProtocol, SignalToken.address);
+    });
+  }
 };
